cc291d8b4ccd07e2b133229a8fae8cac441b2ce8
joelepore <giuseppejoelepore@gmail.com>
2024-11-25 15:56:33 +0100

    style: add useful comments to script.js

--- script.js
+++ script.js
@@ -9,16 +9,18 @@ const discountOver65 = 40;
 
 btnSubmit.addEventListener('click', (e) => {
   e.preventDefault();
-
+  // Prendo i dati di input
   const totKm = parseInt(inputKm.value);
   const age = parseInt(inputAge.value);
-
+  // Eseguo tutti i calcoli che occorrono
   const totalPrice = calculateTotalPrice(totKm, age);
   const cpCode = generateCPCode();
   const seatNumber = generateSeatNumber();
   const cabinNumber = getRandomNumber(1, 10);
+  // Visualizzo l'output
 })
 
+// Funzione che restituisce il prezzo totale del biglietto prendendo in input il totale dei kilometri e l'eta' del passeggero
 function calculateTotalPrice(totKm, age) {
   if (age < 18) {
     discount = discountUnderage;
@@ -33,16 +35,16 @@ function calculateTotalPrice(totKm, age) {
 
   return totalDiscountedPrice;
 }
-
+// Funzione che genera un codice cp random compreso tra 90000 e 99999
 function generateCPCode() {
   return Math.floor(Math.random() * 10000) + 90000;
 }
-
+// Funzione che genera randomicamente il numero del sedile del passeggero (e.g. 1A, 3B, etc.)
 function generateSeatNumber() {
   const seats = ['A', 'B', 'C', 'D', 'E'];
   return `${getRandomNumber(1, 6)}${seats[getRandomNumber(0, seats.length - 1)]}`;
 }
-
+// Funzione che restituisce un intero compreso tra due estremi, estremi inclusi
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
